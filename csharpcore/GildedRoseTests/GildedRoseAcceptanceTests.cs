cfc47e307c67ebcdfa63aaf4d9b9fdabed7213cd
asa-leholland <asaleholland@gmail.com>
2023-09-17 19:22:00 -0700

    test(Build out Acceptance Tests): :white_check_mark: Handle degrade quality below zero

--- csharpcore/GildedRoseTests/GildedRoseAcceptanceTests.cs
+++ csharpcore/GildedRoseTests/GildedRoseAcceptanceTests.cs
@@ -12,7 +12,21 @@ public class GildedRoseAcceptanceTest
     {
         IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         GildedRose app = new GildedRose(items);
+
         app.UpdateQuality();
+
         Assert.Equal("foo", items[0].Name);
+    }
+
+    [Trait("Category", "AcceptanceTest")]
+    [Fact]
+    public void ShouldNeverDegradeQualityBelowZero()
+    {
+        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.Equal(0, items[0].Quality);
     }
 }
