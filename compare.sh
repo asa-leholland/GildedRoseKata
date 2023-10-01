dae0d1898c43e5b7fc25bd8a5a5816be58c0ba57
asa-leholland <asaleholland@gmail.com>
2023-09-30 23:33:55 -0700

    add commit counter

--- compare.sh
+++ compare.sh
@@ -1,27 +1,86 @@
-#!/bin/bash
+import * as vscode from "vscode";
+import Timer from "./Timer";
+import { exec } from 'child_process';
 
-# Specify the target branch
-target_branch="develop"
+const lastcommitconfig = vscode.workspace.getConfiguration("lastcommit");
+let timer: Timer;
+let lastHead: string | null = null;
 
-# Get the name of the current branch
-current_branch=$(git symbolic-ref --short HEAD)
+export async function activate(context: vscode.ExtensionContext) {
+  timer = new Timer();
+  timer.startTimer();
+  vscode.window.showInformationMessage("TimeSince timer has been started!");
 
-# Compare the current branch against the specified target branch
-if [ "$current_branch" != "$target_branch" ]; then
-  git checkout "$target_branch"  # Switch to the specified target branch
-fi
 
-# Get the total insertions and deletions from the last lines of `git diff --shortstat`
-shortstat_output=$(git diff --shortstat 2>/dev/null)
-insertions=$(echo "$shortstat_output" | tail -n 1 | awk '{print $4}')
-deletions=$(echo "$shortstat_output" | tail -n 1 | awk '{print $6}')
 
-# Print the result in the desired format
-if ((insertions + deletions > 0)); then
-  printf "𝜟=%d (+%d/-%d)\n" "$((insertions + deletions))" "$insertions" "$deletions"
-else
-  printf "𝜟=0 (+0/-0)\n"
-fi
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+  statusBarItem.text = "𝜟=0 (+0/-0)"; // Replace with your initial text
+  statusBarItem.show();
 
-# Return to the original branch
-git checkout "$current_branch"
+  setInterval(() => {
+    exec('./output.sh', (error, stdout, stderr) => {
+      if (error) {
+        // Handle errors
+        console.error(error);
+      } else {
+        // Process the script's output and update statusBarItem.text
+        const scriptOutput = stdout.trim(); // Assuming the script outputs the desired text
+        statusBarItem.text = scriptOutput;
+      }
+    });
+  }, 30000);
+
+
+  // Register a command to reset the timer
+  let resetTimer = vscode.commands.registerCommand("lastcommit.resetTimer", () => {
+    timer.resetTimer();
+    vscode.window.showInformationMessage("TimeSince timer has been reset manually!");
+  });
+
+  context.subscriptions.push(resetTimer);
+
+  // Initialize Git commit event listener after a short delay
+  vscode.window.showInformationMessage("Initializing Git commit event listener...");
+  setTimeout(initializeGitCommitListener, 5000); // Delay by 5 seconds
+}
+
+async function initializeGitCommitListener() {
+  const gitExtension = vscode.extensions.getExtension("vscode.git");
+  if (!gitExtension) {
+    vscode.window.showInformationMessage("Git extension not available.");
+    return;
+  }
+
+  const git = gitExtension.exports.getAPI(1);
+
+  if (!git) {
+    vscode.window.showInformationMessage("Git API not available.");
+    return;
+  }
+
+  vscode.window.showInformationMessage("Git extension and API successfully initialized.");
+
+  // Initialize lastHead
+  const repos = git.repositories;
+  if (repos.length > 0) {
+    const repo = repos[0];
+    lastHead = repo.state.HEAD?.commit;
+    vscode.window.showInformationMessage(`Initial HEAD commit hash: ${lastHead}`);
+  }
+
+  // Subscribe to repository change events
+  git.a.onDidChangeRepository(async (repo: any) => {
+    const newHead = repo.state.HEAD?.commit;
+    vscode.window.showInformationMessage(`New HEAD commit hash detected: ${newHead}`);
+
+    if (newHead !== lastHead) {
+      timer.resetTimer();
+      vscode.window.showInformationMessage("TimeSince timer has been reset due to a Git commit.");
+      lastHead = newHead;
+    } else {
+      vscode.window.showInformationMessage("HEAD commit hash unchanged; timer not reset.");
+    }
+  });
+}
+
+export function deactivate() { }
